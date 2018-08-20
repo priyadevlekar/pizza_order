ebdeed6d416513ef9724ca4c819000f4c57fa9ab
Priyanka Devlekar <priyankadevlekar22@gmail.com>
2018-08-20 21:17:26 +0530

    non- veg pizza list added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,54 +5,79 @@
 "use strict";
 window.onload= loadDoc();
 
+var li, img_div, img, order, h4,p;
 var pizza_type = document.getElementById("types");
+var nonveg_type = document.getElementById("types-nonveg");
   function loadDoc() 
   {
       var xobj = new XMLHttpRequest();
       xobj.overrideMimeType("application/json");
       xobj.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        
+      if (this.readyState == 4 && this.status == 200) {   
         var record =JSON.parse(this.responseText);
-        console.log(record);
         call_record(record);
-       
+        console.log(record);
       }
     }
-    xobj.open('GET', 'pizzas.json', true);
+    xobj.open('GET', 'pizza.json', true);
     xobj.send();
   } 
 
     function call_record(record){
-       console.log(record.veg_pizza.length)
-        for (var i = 0; i < 6; i++){
+        for (var i = 0; i < 3; i++){
+      	  li = document.createElement('li');
+          img_div = document.createElement('div');
+          li.setAttribute('class','child-list');
+          li.setAttribute('data-id', record.pizzas['veg_pizza'][i].id);
+          img=document.createElement('img');
+          order=document.createElement('a');
+          order.setAttribute('href','#FIXME');
+          order.setAttribute('title','Order Now');
+          h4 =document.createElement('h4');
+          p = document.createElement('p');
+          
+          img.src=record.pizzas['veg_pizza'][i].img;
 
-          console.log(record.veg_pizza[i].pizza_name);
-        	 var li = document.createElement('li');
-           var img_div = document.createElement('div');
-            li.setAttribute('class','child-list');
-            li.setAttribute('data-id',record.veg_pizza[i].id);
-            var img=document.createElement('img');
-            var order=document.createElement('a');
-            order.setAttribute('href','#FIXME');
-            order.setAttribute('title','Order Now');
-            var h4 =document.createElement('h4');
-            var p = document.createElement('p');
-            
-            img.src=record.veg_pizza[i].img;
-
-            h4.innerHTML = record.veg_pizza[i].pizza_name;
-            console.log(h4);
-             img_div.appendChild(img);
-            p.innerHTML = record.veg_pizza[i].descrition;
-            li.appendChild(img_div);
-            li.appendChild(h4);
-            li.appendChild(p);
-            li.appendChild(order);
-            
-            order.innerHTML = "order Now";
-            pizza_type.appendChild(li);
+          h4.innerHTML = record.pizzas['veg_pizza'][i].pizza_name;
+           
+          img_div.appendChild(img);
+          p.innerHTML = record.pizzas['veg_pizza'][i].descrition;
+          li.appendChild(img_div);
+          li.appendChild(h4);
+          li.appendChild(p);
+          li.appendChild(order);
+          
+          order.innerHTML = "order Now";
+          pizza_type.appendChild(li);
         }
-                 
-            // console.log(record.data[i].id);
-    }
+         
+         for (var i = 0; i < 3; i++){
+          li = document.createElement('li');
+          img_div = document.createElement('div');
+          li.setAttribute('class','child-list');
+          li.setAttribute('data-id', record.pizzas['nonVeg_pizza'][i].id);
+          img=document.createElement('img');
+          order=document.createElement('a');
+          order.setAttribute('href','#FIXME');
+          order.setAttribute('title','Order Now');
+          h4 =document.createElement('h4');
+          p = document.createElement('p');
+          
+          img.src=record.pizzas['nonVeg_pizza'][i].img;
+
+          h4.innerHTML = record.pizzas['nonVeg_pizza'][i].pizza_name;
+           
+          img_div.appendChild(img);
+          p.innerHTML = record.pizzas['nonVeg_pizza'][i].descrition;
+          li.appendChild(img_div);
+          li.appendChild(h4);
+          li.appendChild(p);
+          li.appendChild(order);
+          
+          order.innerHTML = "order Now";
+          nonveg_type.appendChild(li);
+        }        
+         
+    }          
+
+        
