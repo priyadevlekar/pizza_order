47118212da600500a1b7fdf7999ed4c88563d4fd
Priyanka Devlekar <priyankadevlekar22@gmail.com>
2018-08-21 12:42:22 +0530

    more type of pizza functionality added and header with styling added

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ window.onload= loadDoc();
 var li, img_div, img, order, h4,p;
 var pizza_type = document.getElementById("types");
 var nonveg_type = document.getElementById("types-nonveg");
+var item = document.querySelector(".active").innerHTML;
+console.log(item);
+
   function loadDoc() 
   {
       var xobj = new XMLHttpRequest();
@@ -19,65 +22,80 @@ var nonveg_type = document.getElementById("types-nonveg");
         console.log(record);
       }
     }
-    xobj.open('GET', 'pizza.json', true);
+    xobj.open('GET', 'pizzas.json', true);
     xobj.send();
   } 
 
     function call_record(record){
-        for (var i = 0; i < 3; i++){
-      	  li = document.createElement('li');
-          img_div = document.createElement('div');
-          li.setAttribute('class','child-list');
-          li.setAttribute('data-id', record.pizzas['veg_pizza'][i].id);
-          img=document.createElement('img');
-          order=document.createElement('a');
-          order.setAttribute('href','#FIXME');
-          order.setAttribute('title','Order Now');
-          h4 =document.createElement('h4');
-          p = document.createElement('p');
-          
-          img.src=record.pizzas['veg_pizza'][i].img;
+      
+      
+      if(item =='veg pizza' || item =='all types'){
 
-          h4.innerHTML = record.pizzas['veg_pizza'][i].pizza_name;
-           
-          img_div.appendChild(img);
-          p.innerHTML = record.pizzas['veg_pizza'][i].descrition;
-          li.appendChild(img_div);
-          li.appendChild(h4);
-          li.appendChild(p);
-          li.appendChild(order);
-          
-          order.innerHTML = "order Now";
-          pizza_type.appendChild(li);
+
+          for (var i = 0; i < record.pizzas['veg_pizza'].length; i++){
+          if(item=="all types" && i == 3){
+            break;
+          }
+          else {
+
+            li = document.createElement('li');
+            img_div = document.createElement('div');
+            li.setAttribute('class','child-list');
+            li.setAttribute('data-id', record.pizzas['veg_pizza'][i].id);
+            img=document.createElement('img');
+            order=document.createElement('a');
+            order.setAttribute('href','#FIXME');
+            order.setAttribute('title','Order Now');
+            h4 =document.createElement('h4');
+            p = document.createElement('p');
+            
+            img.src=record.pizzas['veg_pizza'][i].img;
+
+            h4.innerHTML = record.pizzas['veg_pizza'][i].pizza_name;
+             
+            img_div.appendChild(img);
+            p.innerHTML = record.pizzas['veg_pizza'][i].descrition;
+            li.appendChild(img_div);
+            li.appendChild(h4);
+            li.appendChild(p);
+            li.appendChild(order);
+            
+            order.innerHTML = "order Now";
+            pizza_type.appendChild(li);
         }
-         
-         for (var i = 0; i < 3; i++){
-          li = document.createElement('li');
-          img_div = document.createElement('div');
-          li.setAttribute('class','child-list');
-          li.setAttribute('data-id', record.pizzas['nonVeg_pizza'][i].id);
-          img=document.createElement('img');
-          order=document.createElement('a');
-          order.setAttribute('href','#FIXME');
-          order.setAttribute('title','Order Now');
-          h4 =document.createElement('h4');
-          p = document.createElement('p');
-          
-          img.src=record.pizzas['nonVeg_pizza'][i].img;
+      }   
+      }
+      if(item =='non-veg pizza' || item =='all types'){
+        for (var i = 0; i < record.pizzas['nonVeg_pizza'].length; i++){
+           if(item=="all types" && i == 3){
+            break;
+          }
+          else {
+            li = document.createElement('li');
+            img_div = document.createElement('div');
+            li.setAttribute('class','child-list');
+            li.setAttribute('data-id', record.pizzas['nonVeg_pizza'][i].id);
+            img=document.createElement('img');
+            order=document.createElement('a');
+            order.setAttribute('href','#FIXME');
+            order.setAttribute('title','Order Now');
+            h4 =document.createElement('h4');
+            p = document.createElement('p');
+            
+            img.src=record.pizzas['nonVeg_pizza'][i].img;
 
-          h4.innerHTML = record.pizzas['nonVeg_pizza'][i].pizza_name;
-           
-          img_div.appendChild(img);
-          p.innerHTML = record.pizzas['nonVeg_pizza'][i].descrition;
-          li.appendChild(img_div);
-          li.appendChild(h4);
-          li.appendChild(p);
-          li.appendChild(order);
-          
-          order.innerHTML = "order Now";
-          nonveg_type.appendChild(li);
+            h4.innerHTML = record.pizzas['nonVeg_pizza'][i].pizza_name;
+             
+            img_div.appendChild(img);
+            p.innerHTML = record.pizzas['nonVeg_pizza'][i].descrition;
+            li.appendChild(img_div);
+            li.appendChild(h4);
+            li.appendChild(p);
+            li.appendChild(order);
+            
+            order.innerHTML = "order Now";
+            nonveg_type.appendChild(li);
         }        
-         
-    }          
-
-        
+      }
+      } 
+    }   
